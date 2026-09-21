205cfb0464a08abac0f0edb3dd1b91b765bc0b6e
rainanirmal <rainanirmal01@gmail.com>
2026-09-21 22:54:58 +0530

    feat: implement user authentication and credential management

--- src/models/users.models.js
+++ src/models/users.models.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new Schema(
     {
@@ -48,6 +50,48 @@ const userSchema = new Schema(
         timestamps: true
     }
 )
+
+userSchema.pre("save", async function (next) {
+
+    if(!this.modified("password")) return next()
+
+    this.password = bcrypt.hash(this.password, 10)
+
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function(password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function() {
+
+    jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function() {
+
+    jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 
 const User = mongoose.model("User", userSchema)
 
