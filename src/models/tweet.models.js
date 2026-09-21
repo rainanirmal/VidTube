9903039d3d2d91c7f02ce72ab911c0b1e0b142cc
rainanirmal <rainanirmal01@gmail.com>
2026-09-21 18:58:41 +0530

    feat: establish tweet data model and schema

---
+++ src/models/tweet.models.js
@@ -0,0 +1,23 @@
+import mongoose,{ Schema } from "mongoose";
+
+const tweetSchema = new Schema(
+    {
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        },
+        content: {
+            type: String,
+            required: true
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+const Tweet = mongoose.model("Tweet", tweetSchema)
+
+export {
+    Tweet
+}
