8470b5e837a9dd40bd71ee2d243ae15c16d196be
rainanirmal <rainanirmal01@gmail.com>
2026-09-21 18:47:14 +0530

    feat: establish like data model and schema

---
+++ src/models/like.models.js
@@ -0,0 +1,39 @@
+// _id string pk
+//   comment ObjectId comments
+//   video ObjectId videos
+//   likedBy ObjectId users
+//   tweet ObjectId tweets
+//   createdAt Date
+//   updatedAt Date
+
+import mongoose, { Schema } from "mongoose";
+
+const likeSchema = new Schema(
+    {
+        video: {
+            type: Schema.Types.ObjectId,
+            ref: "Video"
+        },
+        comment: {
+            type: Schema.Types.ObjectId,
+            ref: "Comment"
+        },
+        tweet: {
+            type: Schema.Types.ObjectId,
+            ref: "Tweet"
+        },
+        likedBy: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+    }, 
+    {
+        timestamps: true
+    }
+)
+
+const Like = mongoose.model("Like", likeSchema)
+
+export {
+    Like
+}
