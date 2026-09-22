4152d64fe5b7000e71ae1e96b95b0e04e606ef5c
rainanirmal <rainanirmal01@gmail.com>
2026-09-22 09:49:19 +0530

    feat: implement cloudinary image upload and local cleanup

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,35 @@
+import { v2 as cloudinary} from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async(localFilePath) => {
+    try {
+        if(!localFilePath) return null
+
+        const response = await cloudinary.uploader.upload(
+            localFilePath, {
+                resource_type: "auto"
+            }
+        )
+
+        console.log("File uploaded on cloudinary, File source : " ,  response.url)
+
+        // once the file is uploaded, delete it from our server
+        fs.unlinkSync(localFilePath)
+
+        return response
+    }
+    catch(error) {
+        fs.unlinkSync(localFilePath)
+        return null
+    }
+}
+
+export {
+    uploadOnCloudinary
+}
