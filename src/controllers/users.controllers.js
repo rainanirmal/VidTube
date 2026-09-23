fe241a4ba41dc436e3e05be694a232825127a313
rainanirmal <rainanirmal01@gmail.com>
2026-09-23 15:51:03 +0530

    feat: implement user avatar update workflow

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -296,10 +296,50 @@ const updateAccountDetails = asyncHandler( async( req, res) => {
 
 })
 
+const updateUserAvatar = asyncHandler( async( req, res) => {
+
+    const avatarLocalPath = req.files?.path
+
+    if(!avatarLocalPath) {
+        throw new ApiError(400, "File is required")
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+
+    if(!avatar.url) {
+        throw new ApiError(500, "Something went wrong while uploading avatar")
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                avatar: avatar.url
+            }
+        },
+        {
+            new: true
+        }
+    ).select("-password -refreshToken")
+
+    return res
+    .status(200)
+    .json(
+        new ApiResoponse(
+            200,
+            user,
+            "Avatar updated successfully !"
+        )
+    )
+})
+
 export {
     registerUser,
     generateAccessAndRefreshToken,
+    refreshAccessToken,
     loginUser,
     logoutUser,
     changeCurrentPassword,
+    updateAccountDetails,
+    updateUserAvatar,
 }
