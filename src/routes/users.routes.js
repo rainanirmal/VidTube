56a4fa96148050643fb3d16e9a7836418b373db9
rainanirmal <rainanirmal01@gmail.com>
2026-09-23 16:12:58 +0530

    feat: wire user controllers to authentication routes

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { registerUser, loginUser, logoutUser } from "../controllers/users.controllers.js";
+import { registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from "../controllers/users.controllers.js";
 import { upload } from "../middlewares/multer.middlewares.js";
 import { verifyJWT } from "../middlewares/auth.controllers.js";
 
 const router = Router()
 
+// unsecure routes
 router.route("/register").post(
     upload.fields([
         {
@@ -19,6 +20,15 @@ router.route("/register").post(
     registerUser)
 
 router.route("/login").post(loginUser)
+router.route("/refresh-token").post(refreshAccessToken)
+
+
+//secure routes
 router.route("/logout").post(verifyJWT, logoutUser)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+
 
 export default router
